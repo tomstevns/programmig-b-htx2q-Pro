e0416558a78c44cdef71f15a6eed9475ad61e56b
tomstevns <tomstevns@gmail.com>
2021-10-08 14:24:55 +0200

    Ny Slagskibsfil. Tilføjet positions funktion Rettet stave fejl loop indtil at xy er korrekt angivet

--- Programeksempler/Lektion12-Slagskibe/main.py
+++ Programeksempler/Lektion12-Slagskibe/main.py
@@ -1,18 +1,33 @@
 
+#
+
+
 def getPos():
-    x = int(input("Indtast x position: \n"))
-    y = int(input("Indtast y position: \n"))
+    tjek = True
+    while tjek:
+        x = int(input("Indtast x position: \n"))
+        if x > 10:
+            print("Dit x kordinat er ikke gyldigt.")
+        else:
+            tjek = False
+    tjek = True
+    while tjek:
+        y = int(input("Indtast y position: \n"))
+        if y > 10:
+            print("Dit y kordinat er ikke gyldigt.")
+        else:
+            tjek = False
+
     position = [x,y]
     return position
 
 def main():
     print("Velkommen til Sænkeslagskibe" )
 
-
     x, y = getPos()
 
-    print(str(x),"Dette er din x Koordinat, " + str(y),"Dette er din y Koordinat \n Angrib dette punkt")
+    print(str(x), "Dette er din x Koordinat, " + str(y), "Dette er din y Koordinat \n Angrib dette punkt")
 
-    print(x , y)
+    print('(' + str(x)+',' + str(y) + ')')
 
 main()
