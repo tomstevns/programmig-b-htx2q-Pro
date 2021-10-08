867cf104e755eccc7ac133a6fc643ef0ddb99bc4
tomstevns <tomstevns@gmail.com>
2021-10-08 13:36:25 +0200

    Ny Slagskibsfil

---
+++ Programeksempler/Lektion12-Slagskibe/main.py
@@ -0,0 +1,14 @@
+
+print("Velkommen til Sænkeslagskibe" )
+
+
+x = input("Hvilken x Koordinat vil du indtaste? \n")
+y = input("Hvilken y Koordinat vil du indtaste? \n")
+
+
+print(x,"Dette er din x Koordinat, " + y,"Dette er din y Koordinat \n Angrib dette punkt")
+
+print(x , y)
+
+
+
