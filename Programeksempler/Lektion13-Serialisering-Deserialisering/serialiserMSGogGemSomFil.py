85a8de9432884f60513a479d08fcd44c35af57bc
tomstevns <tomstevns@gmail.com>
2021-10-12 17:47:45 +0200

    Ny Slagskibsfil. Tilføjet positions funktion Rettet stave fejl Check if kordinaterne er tal og er under 10 ellers repeat input

---
+++ Programeksempler/Lektion13-Serialisering-Deserialisering/serialiserMSGogGemSomFil.py
@@ -0,0 +1,21 @@
+#Definer JSON format
+import json
+
+#definer indhold af besked
+besked = "pass"
+
+#konverter "besked" til et JSON format
+jsonBesked = json.dumps(besked)
+
+# print besked ud på JSON format
+# Læg marke til at variablen "jsonBesked" printes ud som et name:value pair
+print("jsonBesked: ", jsonBesked)
+
+#Gem jsonBesked i en fil
+
+f = open("serializedMessageFile.txt", "a")
+f.write(jsonBesked)
+f.close()
+
+
+
