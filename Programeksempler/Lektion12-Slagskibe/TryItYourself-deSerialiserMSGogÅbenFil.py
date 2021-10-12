bdf248567d2c39240cd4c566ed0f9924a1957d94
tomstevns <tomstevns@gmail.com>
2021-10-12 18:28:16 +0200

    serialiseret og deserialiseret fil inklusive en txt fil som indeholder den serialiserede besked på JSON format

---
+++ Programeksempler/Lektion12-Slagskibe/TryItYourself-deSerialiserMSGogÅbenFil.py
@@ -0,0 +1,49 @@
+"""Du skal nu gøre det modsatte af hvad der blev foretaget i den
+første fil python fil hvor vi serialiserede data
+
+Jeg har fjernet lidt af koden som du selv skal finde ud af
+svarene finde du i "deSerialiserMSGogÅbenFil.py"
+
+De steder hvor der står *****************
+skal du selv tilrette python-koden
+"""
+
+#Definer JSON format
+import *****
+
+#Åben fil
+f = open(****************
+# Læs indhold af fil
+jsonBesked = f.************
+#Luk fil
+f.*************
+
+# print besked ud på JSON format
+# Læg marke til at variablen "jsonBesked" printes ud som et name:value pair
+print("jsonBeskeden er:  ", ***********)
+
+#deserialiser fil
+besked = json.l******************
+
+
+
+#Print den besked som din modspiller har sendt til dig
+print("besked er: ", ****************)
+
+
+
+
+
+
+
+#definer indhold af besked
+besked = {"alt_vel":True}
+
+#konverter "besked" til et JSON format
+jsonBesked = json.dumps(besked)
+
+
+#Gem jsonBesked i en fil
+
+
+
