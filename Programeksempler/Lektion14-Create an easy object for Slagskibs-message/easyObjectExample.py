3559cc10b9787d4728338fadc85c70fa4e569cb1
tomstevns <tomstevns@gmail.com>
2021-10-15 13:41:53 +0200

    Afleveringsopgave1 til 29 Oktober 2021 kl 18:00

--- Programeksempler/Lektion14-Create an easy object for Slagskibs-message/easyObjectExample.py
+++ Programeksempler/Lektion14-Create an easy object for Slagskibs-message/easyObjectExample.py
@@ -5,21 +5,30 @@ class Object:
         return json.dumps(self, default=lambda o: o.__dict__,
             sort_keys=True, indent=4)
 
+
+
+j =""
+class Payload(object):
+    def __init__(self, j):
+        self.__dict__ = json.loads(j)
+
+
+
+
 #instanciate an object me
 me = Object()
 #Add an attribute name to the object me
 me.name = "Frederik"
 #Add an attribute age to the object me
-me.age = 53
+me.mesage = "hej"
 #instanciate an inner-object dog to the object me
-me.dog = Object()
-#Add an attribute name to the object me
-me.dog.name = "Grace"
-#Add an attribute purebredNname to the object me
-me.dog.purebredName = "Border Collie"
+
 #Place two empty lines
 print("\n\n")
 #Print the Object me into as a JSON format
+
+print("me as python")
+print(me)
 print("Print the Object me in JSON format\n\n",me.toJSON())
 #Print Content of the Object me
 print("[Print the attributes of Object me]\n","me.name = ",me.name,"\n","me.age = ",me.age)
@@ -30,3 +39,21 @@ f = open("serializedMessageFile.txt", "w")
 f.flush()
 f.write(me.toJSON())
 f.close()
+
+#Nu lad vi som om at filen er blevet sendt til en modtagers outlook og gemt i en folder
+
+#Modtager åbner og læser indholdet i filen på JSON format
+print("[Denne del af programmet kører normalt i modtagerens deSerialiserings program]")
+f = open("serializedMessageFile.txt", "r")
+filensTxtpåJSONformat = f.read()
+print("printer filens indhold i JSON format","\n\n",filensTxtpåJSONformat)
+modtMe = Object.toPython(filensTxtpåJSONformat)
+
+print(modtMe)
+
+"""#Print Content of the Object me
+print("[Print the attributes of Objec me]\n","modtMe.name = ",modtMe.name,"\n","modtMe.age = ",modtMe.age)
+#Print the attributes of inner-Object dog
+print("[Print the attribute of inner-Object dog]\n","modtMe.dog.name = ", modtMe.dog.name,"\n","modtMe.dog.purebredName = ", modtMe.dog.purebredName, "\n")
+"""
+
