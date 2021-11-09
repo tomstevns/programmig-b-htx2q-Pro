74540995d76eca1e96e81afdd15926c57afd33af
tomstevns <tomstevns@gmail.com>
2021-11-09 13:00:07 +0100

    2 afleveringsopgave3 opgaver

--- Afleveringsopgaver/Afleveringsopgave3/Afleveringsopgave3.py
+++ Afleveringsopgaver/Afleveringsopgave3/Afleveringsopgave3.py
@@ -58,128 +58,22 @@ beregnModulus(10,2.1)
 
 """
 
-#funktionen ganger et tal med syv og returnerer resultatet
-def gangTalMed7(inputTal):
-    #printer input
-    print("input tal er: ", inputTal)
-    #ganger input med 7
-    resultat = inputTal*7
-    #printer resultat
-    print("resultat er: ", resultat)
-    #funktionen gangTalMed7() returnerer resultat
-    return resultat
-
-#test funtion med input tal 10
-gangTalMed7(10)
 
 """Opgave 2:
-- lav en funktion som hedder lægToTalSammen 
+- lav en funktion som heddder findAntalKromosomerIFil(filnavn) 
 - funktionen skal gøre følgende
-    - print input tallene
-    - læg tallene sammen
-    - print resultat
-    -returner resulat
+    - finde antal af karakteren > i filen
+    - print dette antal
+    -returner antal 
     
 test funktion med følgende kode:
 
-lægToTalSammen(10,7)
+#teste funktion med filen opgave2.fasta
+findAntalKromosomerIFil(opgave2.fasta)
+#teste funktion med et filnavn som ikke eksisterer
+findAntalKromosomerIFil(etFilnavnSomIkkeEksisterer)
+ 
+
+
 """
-
-#funktionen adderer to tal og returnerer resultatet
-def lægToTalSammen(inputTal1,inputTal2):
-    print("input tal1 er: ", inputTal1)
-    print("input tal2 er: ", inputTal2)
-    # Lav resten selv
-
-#lægToTalSammen()
-
-
-"""Opgave 3:
-- lav en funktion som hedder lægToStrengeSammen 
-- funktionen skal gøre følgende
-    - print input strengene
-    - læg strengene sammen
-    - print resultat
-    -returner resulat
-    
-test funktion med følgende kode:
-
-lægToStrengeSammen("Vi har efterårsferie i uge","42")
-"""
-
-#funktionen adderer to strenge og returnerer resultatet
-def lægToStrengeSammen(inputStreng1,inputStreng2):
-    # Lav resten selv
-    print("")
-
-lægToStrengeSammen("a","b")
-
-"""Opgave 4:
-- lav en funktion som hedder talletErStørreEndTI 
-- funktionen skal gøre følgende
-    - print input tal
-    - lav en IF sætning som sætter resultat til True hvis tallet er større end 10
-    - lav en Else sætning som sætter resultat til False hvis tallet er mindre end 10
-    - print resultat
-    -returner resulat
-    
-test funktion med følgende kode:
-
-talletErStørreEndTI(11)
-talletErStørreEndTI(10)
-talletErStørreEndTI(9)
-talletErStørreEndTI(-11)
-"""
-
-
-def talletErStørreEndTI(dummy):
-    # Lav resten selv
-    print("")
-
-#tester funktionen med følgende kode:
-
-talletErStørreEndTI(23423)
-    # Lav resten selv
-
-
-
-"""Opgave 5:
-- lav en funktion som hedder printInputværdiFraInputVærdiTilNul  
-- funktionen skal gøre følgende
-    
-    - print inputværdi
-    - resultat = resultat + inputværdi
-    - print inputværdi-1
-    - resultat = resultat + inputværdi
-    - print inputværdi-2
-    - resultat = resultat + inputværdi
-    etc.......
-    - print inputværdi - inputværdi
-    - resultat = resultat + inputværdi
-    -returner resulat
-    
-test funktion med følgende kode:
-
-printInputværdiFraInputVærdiTilNul(10)
-"""
-
-# Opgave 5 laves her
-
-
-"""Opgave 6:
-- lav en funktion som hedder getValutasKurs  
-- funktionen skal gøre følgende
-    - kunne håndtere at returnere valutakursen for følgende valutakoder
-      JPY, RAN , DKK, EUR, GBP
-    - kunne håndtere at returnere en fejl besked for valutakoder som funktionen ikke kender på forhånd
-    - print inputværdi
-    - resultat = valutakodens tilhørende kursværdi 
-    - print resultat
-    -returner resulat
-    
-test funktion med følgende kode med inputværdierne: JPY, RAN , DKK, EUR, GBP, NOK, USD
-"""
-
-# Opgave 6 laves her
-
 
