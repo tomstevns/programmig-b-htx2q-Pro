a8c7ed89c5a0bb31d1bd3888c3e5b9a1c9ce7ee0
tomstevns <tomstevns@gmail.com>
2021-11-26 10:15:58 +0100

    Afleveringsopgave4

--- Afleveringsopgaver/Afleveringsopgave4/Afleveringsopgave4.py
+++ Afleveringsopgaver/Afleveringsopgave4/Afleveringsopgave4.py
@@ -1,10 +1,34 @@
 #Opgave 1
 #Lav et program som returnerer længden af en streng
 
+def findLængdeAfStreng(inputStreng):
+    strengLængde = len(inputStreng)
+    return strengLængde
+#Test af opgave1
+testStreng = "abcdef340958304g"
+print("længden af strengen ",testStreng," er ", findLængdeAfStreng(testStreng)," karakterer")
+print("længden af strengen ", testStreng," er ",findLængdeAfStreng(testStreng), " karakterer")
+
 
 #Opgave2
 #Lav et program åbner en fil1, skriver "hej med dig" i filen 13153 gange, men den
 # 5.158 gang skrives der "farvel med dig"
+
+#Definer og åben en fil
+f = open("fil1", "r")
+
+
+
+#Lav et loop som skriver "hej med dig" 13,153
+for i in range(13)
+#IF I er 5158 så
+print("I skal nu være 5158")
+#Skrive i filen - "Farvel med dig"
+#ELSE
+#Skrive i filen - "Hej med dig"
+
+
+
 
 
 #Opgave3
