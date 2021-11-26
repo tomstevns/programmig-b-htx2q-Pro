bcadb35ff4d34cbeb774a382d3dccc7bbbafb0a9
tomstevns <tomstevns@gmail.com>
2021-11-26 10:55:20 +0100

    Afleveringsopgave4

--- Afleveringsopgaver/Afleveringsopgave4/Afleveringsopgave4.py
+++ Afleveringsopgaver/Afleveringsopgave4/Afleveringsopgave4.py
@@ -17,17 +17,19 @@ print("længden af strengen ", testStreng," er ",findLængdeAfStreng(testStreng)
 #Definer og åben en fil
 f = open("fil1", "r")
 
-
-
-#Lav et loop som skriver "hej med dig" 13,153
+#Lav et loop 13153 gange som skriver
 for i in range(13)
-#IF I er 5158 så
-print("I skal nu være 5158")
-#Skrive i filen - "Farvel med dig"
-#ELSE
-#Skrive i filen - "Hej med dig"
+    #IF I er 5158 så
+    print("I skal nu være 5158")
+    #Skrive i filen - "Farvel med dig"
+    #Noget med
+    f.write("truttelut")
+    #ELSE
+    #Skrive i filen - "Hej med dig"
+    #Noget med
+    f.write("vuffelivuf")
 
-
+#Luk filen fil1
 
 
 
