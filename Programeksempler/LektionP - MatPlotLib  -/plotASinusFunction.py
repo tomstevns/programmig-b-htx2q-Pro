de2fcfecb9948b5644e76836eedc3120ca7076ec
tomstevns <tomstevns@gmail.com>
2022-01-07 14:59:09 +0100

    LektionP MathplotLib

--- Programeksempler/LektionP - MatPlotLib  -/plotASinusFunction.py
+++ Programeksempler/LektionP - MatPlotLib  -/plotASinusFunction.py
@@ -3,13 +3,13 @@ import numpy as np
 
 # 100 linearly spaced numbers
 
-x = np.linspace(-np.pi, np.pi, 1000)
+x = np.linspace(-np.pi, np.pi, 1000000)
 
 # the function, which is y = sin(x)
 y = np.sin(x)
-#mystisk = (np.sin(x*500)/300)+(np.sin(x*2000)/1000)+(np.sin(x*20000)/10000)+(np.sin(x*200000)/100000)+0.0001
+mystisk = (np.sin(x*500)/300)+(np.sin(x*2000)/1000)+(np.sin(x*20000)/10000)+(np.sin(x*200000)/100000)+0.0001
 
-mystisk = (np.sin(x*500)/300)+(np.sin(x*2000)/1000)+0.0001
+#mystisk = (np.sin(x*500)/300)+(np.sin(x*2000)/1000)+0.0001
 
 # setting the axes at the centre
 fig = plt.figure()
@@ -25,6 +25,7 @@ ax.yaxis.set_ticks_position('left')
 plt.plot(x, y, 'r', label='En sinus funktion')
 plt.plot(x, y / 2, 'b', label='En sinus/2 funktion')
 plt.plot(x,(x+mystisk),"g", label = "test")
+plt.legend(loc='upper left')
 
 # show the plot
 plt.show()
---
+++ Programeksempler/LektionP - MatPlotLib  -/plotDistanceByAcceleration.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 100 linearly spaced numbers
+x = np.linspace(0,10,100)
+
+# the function, which is y = x^2 here
+y = 0.5*9.12*pow(x,2)
+
+# setting the axes at the centre
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('zero')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# plot the function
+plt.plot(x,y, 'r', label = "Distance")
+plt.legend(loc='upper left')
+plt.grid()
+# show the plot
+plt.show()
+
